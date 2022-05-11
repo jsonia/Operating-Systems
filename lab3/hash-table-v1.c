fcc76891bc10ea65ea68839ad3498dd152741380
soniajaiswal <soniajaiswal@g.ucla.edu>
2022-05-10 19:43:23 -0700

    v1 modified

--- lab3/hash-table-v1.c
+++ lab3/hash-table-v1.c
@@ -4,6 +4,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <sys/queue.h>
+#include <pthread.h>
 
 struct list_entry {
 	const char *key;
@@ -19,6 +20,7 @@ struct hash_table_entry {
 
 struct hash_table_v1 {
 	struct hash_table_entry entries[HASH_TABLE_CAPACITY];
+    static pthread_mutex_t foo_mutex;
 };
 
 struct hash_table_v1 *hash_table_v1_create()
@@ -29,6 +31,7 @@ struct hash_table_v1 *hash_table_v1_create()
 		struct hash_table_entry *entry = &hash_table->entries[i];
 		SLIST_INIT(&entry->list_head);
 	}
+    pthread_mutex_init(&foo_mutex, NULL)
 	return hash_table;
 }
 
@@ -69,7 +72,7 @@ bool hash_table_v1_contains(struct hash_table_v1 *hash_table,
 void hash_table_v1_add_entry(struct hash_table_v1 *hash_table,
                              const char *key,
                              uint32_t value)
-{
+{   pthread_mutex_lock(&foo_mutex);
 	struct hash_table_entry *hash_table_entry = get_hash_table_entry(hash_table, key);
 	struct list_head *list_head = &hash_table_entry->list_head;
 	struct list_entry *list_entry = get_list_entry(hash_table, key, list_head);
@@ -84,6 +87,7 @@ void hash_table_v1_add_entry(struct hash_table_v1 *hash_table,
 	list_entry->key = key;
 	list_entry->value = value;
 	SLIST_INSERT_HEAD(list_head, list_entry, pointers);
+    pthread_mutex_unlock(&foo_mutex);
 }
 
 uint32_t hash_table_v1_get_value(struct hash_table_v1 *hash_table,
@@ -108,5 +112,6 @@ void hash_table_v1_destroy(struct hash_table_v1 *hash_table)
 			free(list_entry);
 		}
 	}
+    pthread_mutex_destroy(&foo_mutex);
 	free(hash_table);
 }
