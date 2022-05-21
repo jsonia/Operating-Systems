40274802119df327f490617e32c346366db6629a
soniajaiswal <soniajaiswal@g.ucla.edu>
2022-05-20 17:02:05 -0700

    final

--- lab3/hash-table-v2.c
+++ lab3/hash-table-v2.c
@@ -76,11 +76,11 @@ void hash_table_v2_add_entry(struct hash_table_v2 *hash_table,
 {
 	struct hash_table_entry *hash_table_entry = get_hash_table_entry(hash_table, key);
     /*lock here*/
-    
-	struct list_head *list_head = &hash_table_entry->list_head;
-	struct list_entry *list_entry = get_list_entry(hash_table, key, list_head);
     int err = pthread_mutex_lock(&hash_table_entry->foo_mutex); //adding err
     if(err) exit(err);
+	struct list_head *list_head = &hash_table_entry->list_head;
+	struct list_entry *list_entry = get_list_entry(hash_table, key, list_head);
+    
 	/* Update the value if it already exists */
 	if (list_entry != NULL) {
 		list_entry->value = value;
